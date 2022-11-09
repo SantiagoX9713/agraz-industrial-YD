9158a3d86d7012bb53b063f90efb951afac8f5e8
Santiago Ramos Morales <sramos@estrasol.com.mx>
2022-11-08 21:42:04 -0600

    Gauge pendiente

--- YDdata/my_tk.py
+++ YDdata/my_tk.py
@@ -3,41 +3,10 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk, messagebox
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from .get_csv import DfFromCsv
+import plotly.graph_objects as go
 
 
-def create_plot(container, prepared_data):
-    # for widget in container.winfo_children():
-    #     widget.destroy()
-    
-    frame = ttk.Frame(container)
-    frame.columnconfigure(0, weight=1)
-    frame.winfo_name = 'PLOTER'
-    fig = plt.Figure(figsize=(7,5), dpi=80)
-    fig.patch.set_facecolor('black')
-    ax = fig.add_subplot()
-    df2 = prepared_data[['Time','Solt Mec']]
-    colors = ["red" if i > 2.72 else "yellow" for i in df2['Solt Mec']]
-    df2.plot.line(legend=True, ax=ax, color=colors, fontsize=16, grid=True)
-    ax.axhline(y=2.81, color='r', linestyle='-')
-    ax.axhline(y=2.70, color='b', linestyle='-')
-    ax.set_title('Solturas Mecánicas')
-    ax.set_ylim([2.6,2.85])
-    ax.set_facecolor('black')
-    [t.set_color('white') for t in ax.xaxis.get_ticklines()]
-    [t.set_color('white') for t in ax.xaxis.get_ticklabels()]
-    [t.set_color('white') for t in ax.yaxis.get_ticklines()]
-    [t.set_color('white') for t in ax.yaxis.get_ticklabels()]
-    ax.spines['bottom'].set_color('white')
-    ax.spines['top'].set_color('white')
-    ax.spines['right'].set_color('white')
-    ax.spines['left'].set_color('white')
-    canvas = FigureCanvasTkAgg(fig, master=frame)  # A tk.DrawingArea.
-    canvas.draw()
-    toolbar = NavigationToolbar2Tk(canvas, frame, pack_toolbar=False)
-    toolbar.update()
-    toolbar.pack(side=tk.BOTTOM, fill=tk.X)
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-    frame.place(x=0, y=50)
 
 
 
@@ -48,27 +17,118 @@ class MyTk(tk.Tk):
         self.state("zoomed")
         self.wm_title("YoDiagnostico")
         
+        self.prepared_data = DfFromCsv()
         barra_menus = tk.Menu()
         barra_menus.winfo_name = 'MENU'
         menu_archivo = tk.Menu(barra_menus, tearoff=False)
         barra_menus.add_cascade(menu=menu_archivo, label="Principal")
 #        menu_archivo.add_command(label="Salir", command=self.destroy)
         menu_archivo.add_command(label="Canal 1", command=self.channel_1)
+        menu_archivo.add_command(label="Canal 2", command=self.channel_2)
+        menu_archivo.add_command(label="Canal 3", command=self.channel_3)
         menu_archivo.add_command(label="Canal 4", command=self.channel_4)
+        menu_archivo.add_command(label="Gauge", command=self.gauge)
         menu_archivo.add_command(label="Salir", command=self.destroy)
         self.config(menu=barra_menus)
         
-    
+    def create_plot(self,container, prepared_data):
+        # for widget in container.winfo_children():
+        #     widget.destroy()
+
+        frame = ttk.Frame(container)
+        frame.columnconfigure(0, weight=1)
+        frame.winfo_name = 'PLOTER'
+        fig = plt.Figure(figsize=(7,5), dpi=120)
+        fig.patch.set_facecolor('black')
+        ax = fig.add_subplot()
+        df2 = prepared_data[['Time','Solt Mec']]
+        colors = ["red" if i > 2.72 else "yellow" for i in df2['Solt Mec']]
+        df2.plot.line(legend=True, ax=ax, color=colors, fontsize=16, grid=True)
+        ax.axhline(y=2.81, color='r', linestyle='-')
+        ax.axhline(y=2.70, color='b', linestyle='-')
+        ax.set_title('Solturas Mecánicas')
+        ax.set_ylim([2.6,2.85])
+        ax.set_facecolor('black')
+        [t.set_color('white') for t in ax.xaxis.get_ticklines()]
+        [t.set_color('white') for t in ax.xaxis.get_ticklabels()]
+        [t.set_color('white') for t in ax.yaxis.get_ticklines()]
+        [t.set_color('white') for t in ax.yaxis.get_ticklabels()]
+        ax.spines['bottom'].set_color('white')
+        ax.spines['top'].set_color('white')
+        ax.spines['right'].set_color('white')
+        ax.spines['left'].set_color('white')
+        canvas = FigureCanvasTkAgg(fig, master=frame)  # A tk.DrawingArea.
+        canvas.draw()
+        toolbar = NavigationToolbar2Tk(canvas, frame, pack_toolbar=False)
+        toolbar.update()
+        toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        frame.place(x=0, y=50)
+
+    def create_gauge(self, container):
+        frame = ttk.Frame(container)
+        frame.columnconfigure(0, weight=1)
+        fig = go.Figure(go.Indicator(
+            mode = "gauge+number",
+            value = 270,
+            domain = {'x': [0, 1], 'y': [0, 1]},
+            title = {'text': "Speed"}))
+        canvas = FigureCanvasTkAgg(fig, master=frame)  # A tk.DrawingArea.
+        canvas.draw()
+        toolbar = NavigationToolbar2Tk(canvas, frame, pack_toolbar=False)
+        toolbar.update()
+        toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        frame.place(x=0, y=50)
+
+
+    def gauge(self):
+        for widget in self.winfo_children():
+            if widget.winfo_name != 'MENU':
+                print(widget.winfo_name)
+                widget.destroy()
+        self.wm_title("YoDiagnostico | Gauge")
+        self.create_gauge(self)
+        print('Channel 1')
+
+
     def channel_1(self):
         for widget in self.winfo_children():
             if widget.winfo_name != 'MENU':
                 print(widget.winfo_name)
                 widget.destroy()
+        self.wm_title("YoDiagnostico | Canal 1")
+        self.create_plot(self, self.prepared_data.__getattribute__('canal1'))
         print('Channel 1')
 
+    
     def channel_4(self):
         for widget in self.winfo_children():
-            widget.destroy()
+            if widget.winfo_name != 'MENU':
+                print(widget.winfo_name)
+                widget.destroy()
+        self.wm_title("YoDiagnostico | Canal 2")
+        self.create_plot(self, self.prepared_data.__getattribute__('canal2'))
+        print('Channel 4')
+    
+    
+    def channel_2(self):
+        for widget in self.winfo_children():
+            if widget.winfo_name != 'MENU':
+                print(widget.winfo_name)
+                widget.destroy()
+        self.wm_title("YoDiagnostico | Canal 3")
+        self.create_plot(self, self.prepared_data.__getattribute__('canal3'))
+        print('Channel 4')
+    
+    
+    def channel_3(self):
+        for widget in self.winfo_children():
+            if widget.winfo_name != 'MENU':
+                print(widget.winfo_name)
+                widget.destroy()
+        self.wm_title("YoDiagnostico | Canal 4")
+        self.create_plot(self, self.prepared_data.__getattribute__('canal4'))
         print('Channel 4')
 
 
@@ -91,6 +151,10 @@ class Application(ttk.Frame):
             title="Nuevo elemento seleccionado",
             message=selection
         )
+
+
+
+
 
 
 def data_selector(container):
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ except:
     pass
 
 
-from YDdata.get_csv import GetCsvFtp, DfFromCsv
-from YDdata.my_tk import MyTk, create_plot, data_selector
+from YDdata.get_csv import GetCsvFtp
+from YDdata.my_tk import MyTk
 
 # def filter_by_time(df,h):
 #     now = datetime.now()
@@ -17,10 +17,10 @@ from YDdata.my_tk import MyTk, create_plot, data_selector
 # df = filter_by_time(canal1, 10)
 getter = GetCsvFtp()
 getter.conect()
-prepared_data = DfFromCsv()
+
 root = MyTk()
 # channel_selector = data_selector(root)
 # channel_selector.place(x=0, y=0)
-create_plot(root, prepared_data.__getattribute__('canal1'))
+# root.create_plot(root)
 # rec.place(x=0, y=50)
 root.mainloop()
